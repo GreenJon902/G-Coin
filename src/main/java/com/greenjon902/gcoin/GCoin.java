02ff845e8ec3e857b0563a6c67246d23a24be64b
greenjon902 <greenjon902@gmail.com>
2022-05-02 15:20:19 +0100

    added some logging stuff

--- src/main/java/com/greenjon902/gcoin/GCoin.java
+++ src/main/java/com/greenjon902/gcoin/GCoin.java
@@ -2,16 +2,22 @@ package com.greenjon902.gcoin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class GCoin extends JavaPlugin {
+    Logger logger = this.getLogger();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        logger.info("G-Coin starting...");
 
+
+
+        logger.info("G-Coin started!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        logger.info("G-Coin stopping!")
     }
 }
