bb9d4ad2798cfbddb179c1762637519f4c1be86f
greenjon902 <greenjon902@gmail.com>
2022-05-08 15:08:50 +0100

    made it all in a centralized function

--- src/main/java/com/greenjon902/gcoin/GCoin.java
+++ src/main/java/com/greenjon902/gcoin/GCoin.java
@@ -14,9 +14,13 @@ import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 public final class GCoin extends JavaPlugin implements Listener {
@@ -25,19 +29,9 @@ public final class GCoin extends JavaPlugin implements Listener {
     private static final int gcoinCustomModelData = 71;
 
     private static ItemStack gCoinItemStack;
-    private static ItemStack gCoinNoteItemStack;
-    private static ItemStack gCoinBlockItemStack;
 
     public static ItemStack getGCoinItemStack() {
         return gCoinItemStack;
-    }
-
-    public static ItemStack getGCoinNoteItemStack() {
-        return gCoinNoteItemStack;
-    }
-
-    public static ItemStack getGCoinBlockItemStack() {
-        return gCoinBlockItemStack;
     }
 
     @Override
@@ -54,47 +48,62 @@ public final class GCoin extends JavaPlugin implements Listener {
         gCoinItemMeta.displayName(Component.text("G-Coin", TextColor.color(255, 170, 0)).asComponent());
         gCoinItemStack.setItemMeta(gCoinItemMeta);
 
-        gCoinNoteItemStack = new ItemStack(Material.GOLD_INGOT);
-        ItemMeta gCoinNoteItemMeta = gCoinNoteItemStack.getItemMeta();
-        gCoinNoteItemMeta.setCustomModelData(gcoinCustomModelData);
-        gCoinNoteItemMeta.displayName(Component.text("", TextColor.color(255, 170, 0)).asComponent());
-        gCoinNoteItemStack.setItemMeta(gCoinNoteItemMeta);
+        ArrayList<CraftingHelper> craftingHelpers = new ArrayList<>() {{
+            add(new CraftingHelper("G-Coin", Material.SUNFLOWER, 0));
+            add(new CraftingHelper("5 G-Coins Note", Material.GOLD_INGOT, 5));
+            add(new CraftingHelper("10 G-Coins Note", Material.GOLD_INGOT, 2));
+            add(new CraftingHelper("20 G-Coins Note", Material.GOLD_INGOT, 2));
+            add(new CraftingHelper("G-Block", Material.GOLD_BLOCK, 9));
+        }};
+        HashMap<String, ItemStack> items = new HashMap<>() {{
+           put("G-Coin", gCoinItemStack);
+        }};
 
-        gCoinBlockItemStack = new ItemStack(Material.GOLD_BLOCK);
-        ItemMeta gCoinBlockItemMeta = gCoinBlockItemStack.getItemMeta();
-        gCoinBlockItemMeta.setCustomModelData(gcoinCustomModelData);
-        gCoinBlockItemMeta.displayName(Component.text("G-Block", TextColor.color(255, 170, 0)).asComponent());
-        gCoinBlockItemStack.setItemMeta(gCoinBlockItemMeta);
+        for (CraftingHelper craftingHelper : craftingHelpers) {
+            ItemStack itemStack = new ItemStack(craftingHelper.material);
+            ItemMeta itemMeta = itemStack.getItemMeta();
+            itemMeta.setCustomModelData(gcoinCustomModelData);
+            itemMeta.displayName(Component.text(craftingHelper.name, TextColor.color(255, 170, 0)).asComponent());
+            itemStack.setItemMeta(itemMeta);
+
+            items.put(craftingHelper.name, itemStack);
+        }
+
+        ItemStack last = items.get("G-Coin");
+        for (int i = 1; i < craftingHelpers.size(); i++) {
+            CraftingHelper craftingHelper = craftingHelpers.get(i);
+            getLogger().info("Adding recipe for " + craftingHelper.name + " from " + last.toString());
+
+            ItemStack itemStack = items.get(craftingHelper.name);
 
 
-        ShapedRecipe gCoinNoteShapedRecipe = new ShapedRecipe(new NamespacedKey(this,
-                "gcoin_note"), gCoinNoteItemStack);
-        gCoinNoteShapedRecipe.shape("C C", "CCC");
-        gCoinNoteShapedRecipe.setIngredient('C', new RecipeChoice.ExactChoice(gCoinItemStack));
+            String corrected_name = craftingHelper.name.toLowerCase(Locale.ROOT);
+            corrected_name = corrected_name.replace(" ", "_");
+            ShapelessRecipe shapelessRecipe = new ShapelessRecipe(new NamespacedKey(this, "gcoin_craft_for_" + corrected_name), itemStack);
+            shapelessRecipe.addIngredient(craftingHelper.amountOfLast, last);
 
-        ItemStack gNoteOut = gCoinNoteItemStack.clone();
-        gNoteOut.setAmount(9);
-        ShapedRecipe gCoinNoteShapedRecipe2 = new ShapedRecipe(new NamespacedKey(this,
-                "gcoin_note_2"), gNoteOut);
-        gCoinNoteShapedRecipe2.shape("B");
-        gCoinNoteShapedRecipe2.setIngredient('B', new RecipeChoice.ExactChoice(gCoinBlockItemStack));
+            Bukkit.addRecipe(shapelessRecipe);
+            last = itemStack;
+        }
 
-        ShapedRecipe gCoinBlockShapedRecipe = new ShapedRecipe(new NamespacedKey(this,
-                "gcoin_block"), gCoinBlockItemStack);
-        gCoinBlockShapedRecipe.shape("NNN", "NNN", "NNN");
-        gCoinBlockShapedRecipe.setIngredient('N', new RecipeChoice.ExactChoice(gCoinNoteItemStack));
+        last = items.get("G-Coin");
+        for (int i = 1; i < craftingHelpers.size(); i++) {
+            CraftingHelper craftingHelper = craftingHelpers.get(i);
+            getLogger().info("Adding recipe from " + craftingHelper.name + " to " + last.toString());
 
-        ItemStack gCoinOut = gCoinItemStack.clone();
-        gCoinOut.setAmount(5);
-        ShapedRecipe gCoinShapedRecipe = new ShapedRecipe(new NamespacedKey(this,
-                "gcoin"), gCoinOut);
-        gCoinShapedRecipe.shape("N");
-        gCoinShapedRecipe.setIngredient('N', new RecipeChoice.ExactChoice(gCoinNoteItemStack));
+            ItemStack itemStack = items.get(craftingHelper.name);
 
-        Bukkit.addRecipe(gCoinNoteShapedRecipe);
-        Bukkit.addRecipe(gCoinNoteShapedRecipe2);
-        Bukkit.addRecipe(gCoinBlockShapedRecipe);
-        Bukkit.addRecipe(gCoinShapedRecipe);
+            ItemStack last_ = last.clone();
+            last_.setAmount(craftingHelper.amountOfLast);
+
+            String corrected_name = craftingHelper.name.toLowerCase(Locale.ROOT);
+            corrected_name = corrected_name.replace(" ", "_");
+            ShapelessRecipe shapelessRecipe = new ShapelessRecipe(new NamespacedKey(this, "gcoin_craft_from_" + corrected_name), last_);
+            shapelessRecipe.addIngredient(1, itemStack);
+
+            Bukkit.addRecipe(shapelessRecipe);
+            last = itemStack;
+        }
 
         logger.info("G-Coin started!");
     }
@@ -153,5 +162,17 @@ public final class GCoin extends JavaPlugin implements Listener {
                 event.getInventory().getItem(0).getItemMeta().getCustomModelData() == gcoinCustomModelData) {
             event.setCancelled(false);
         }
+    }
+}
+
+class CraftingHelper {
+    public final String name;
+    public final Material material;
+    public final int amountOfLast;
+
+    CraftingHelper(String name, Material material, int amountOfLast) {
+        this.name = name;
+        this.material = material;
+        this.amountOfLast = amountOfLast;
     }
 }
